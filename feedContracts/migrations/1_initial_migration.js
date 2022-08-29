6d1830395da8e8f3adbef31630099f75c7286cb8
My Name <hariprakash@sparkouttech.com>
2022-08-29 16:57:49 +0530

    fixes

--- feedContracts/migrations/1_initial_migration.js
+++ feedContracts/migrations/1_initial_migration.js
@@ -4,6 +4,6 @@ const MultipleResponse = artifacts.require("MultipleResponse")
 
 module.exports = function (deployer) {
   deployer.deploy(Feed);
-  deployer.deploy(SingleResponse);
-  deployer.deploy(MultipleResponse);
+  // deployer.deploy(SingleResponse);
+  // deployer.deploy(MultipleResponse);
 };
